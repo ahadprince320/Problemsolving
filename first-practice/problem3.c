a2a2b7ed0c203268525c4c85359d2955a7c1c809
MD.Prins Ahad <151132033+ahadprince320@users.noreply.github.com>
2025-10-07 08:38:42 -0700

    third update problem three-five

---
+++ first-practice/problem3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    
+    int num1= 10;
+    num1=20;
+    num1=50;
+    float f = 4.56;
+    char c= '2';
+    printf("%d \t",num1 );
+    printf("%.1f \t",f);
+    printf("%c",c);
+    return 0;
+}
