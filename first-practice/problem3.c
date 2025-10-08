425427de3849f0c8e771f5a79d3171654e6fffb9
MD.Prins Ahad <151132033+ahadprince320@users.noreply.github.com>
2025-10-08 12:19:14 -0700

    fourth update problem second practice

--- first-practice/problem3.c
+++ first-practice/problem3.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main(){
     
-    int num1= 10;
-    num1=20;
-    num1=50;
-    float f = 4.56;
-    char c= '2';
+    int num1;
+    num1;
+    num1;
+    // float f = 4.56;
+    // char c= '2';
+    scanf("%d",&num1);
+    scanf("%d",&num1);
+    scanf("%d",&num1);
     printf("%d \t",num1 );
-    printf("%.1f \t",f);
-    printf("%c",c);
+    // printf("%.1f \t",f);
+    // printf("%c",c);
     return 0;
 }
---
+++ second-practice/quiztest/quiztest.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+// int main(){
+//     int a=15%4;
+//     printf("%d",a);
+//     return 0;
+// }
+// int main(){
+//     double a= 15/4;
+//     printf("%lf",a);
+//     return 0;
+// }
+
+// int main (){
+//     int a=6;
+//     if(a>5){
+//          printf("hi");
+//     }else{
+//        printf("bye");
+//     }
+// }
+int main(){
+    int a=10;
+    int b= 20;
+    if(a>=b ||a<=b){
+         printf("hi ");
+    }
+    printf("hello");
+}
