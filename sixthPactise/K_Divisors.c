cbafe60a62b8d0e11c9a2d64d5df0336a2f75c41
MD.Prins Ahad <151132033+ahadprince320@users.noreply.github.com>
2025-10-17 04:38:29 -0700

    modialsixfive

--- sixthPactise/K_Divisors.c
+++ sixthPactise/K_Divisors.c
@@ -4,12 +4,12 @@ int main(){
     scanf ("%d",&N);
     for (int i = 1; i <=N; i++)
     {
-       //printf("%d",i);
-       while (N%i==0)
+       if (N%i==0)
        {
-        printf("%d\n",i);
-        break;
+                printf("%d\n",i);
+
        }
+        
        
               
     }
